1d722d69398aa32b3176ed5a49baa2ac5ff83b6d
cjstehno <chris@stehno.com>
2017-04-07 09:15:23 -0500

    hamming

---
+++ hamming/src/main/java/Hamming.java
@@ -0,0 +1,22 @@
+public class Hamming {
+
+    private final int distance;
+
+    public Hamming(final String strandA, final String strandB) {
+        if (strandA.length() != strandB.length()) throw new IllegalArgumentException();
+
+        int diff = 0;
+
+        for (int i = 0; i < strandA.length(); i++) {
+            if (strandA.charAt(i) != strandB.charAt(i)) {
+                diff++;
+            }
+        }
+
+        distance = diff;
+    }
+
+    public int getHammingDistance() {
+        return distance;
+    }
+}
