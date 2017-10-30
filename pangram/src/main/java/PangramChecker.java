697c50eb243e62c17afedeb0953a944bbdcf4632
cjstehno <chris@stehno.com>
2017-10-30 12:57:51 -0500

    updated pangram

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -5,7 +5,6 @@ public class PangramChecker {
     private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public boolean isPangram(final String input) {
-        final String upperInput = input.toUpperCase();
-        return LETTERS.chars().mapToObj(i -> valueOf((char) i)).allMatch(upperInput::contains);
+        return LETTERS.chars().mapToObj(i -> valueOf((char) i)).allMatch(input.toUpperCase()::contains);
     }
 }
